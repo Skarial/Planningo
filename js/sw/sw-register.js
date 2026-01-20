78057d84b18fbeef748cb0e3567495b4e85ec9df
CHATELAIN JORDAN <jordan139@hotmail.fr>
2026-01-20 17:56:45 +0100

    fix(pwa): export registerServiceWorker

---
+++ js/sw/sw-register.js
@@ -0,0 +1,20 @@
+// js/sw/sw-register.js
+
+export function registerServiceWorker() {
+  if (!("serviceWorker" in navigator)) return;
+
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    return;
+  }
+
+  navigator.serviceWorker
+    .register("/planning_pwa/service-worker.js", {
+      scope: "/planning_pwa/",
+    })
+    .then((reg) => {
+      console.log("[SW] enregistré");
+    })
+    .catch((err) => {
+      console.error("[SW] échec enregistrement", err);
+    });
+}
