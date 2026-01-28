dcf57fa0537b81d484f3cf489097181fac5aa86d
CHATELAIN JORDAN <jordan139@hotmail.fr>
2026-01-28 11:24:59 +0100

    Correction fichier module generateur

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
   Application Planning PWA
 */
-export const APP_VERSION = "1.0.112";
+export const APP_VERSION = "1.0.114";
 
 import { getConfig } from "./data/db.js";
 import { showActivationScreen } from "./components/activationScreen.js";
@@ -98,6 +98,8 @@ async function watchServiceWorkerUpdates() {
     });
   });
 }
+
+
 
 
 
