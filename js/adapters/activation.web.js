e5fc3f4c54e182fab9efbe23a972917c1d7935d4
CHATELAIN JORDAN <jordan139@hotmail.fr>
2026-01-28 10:51:36 +0100

    Modification activation.js

--- js/adapters/activation.web.js
+++ js/adapters/activation.web.js
@@ -1,24 +1,10 @@
 import { validateActivation } from "../domain/activation.js";
-
-const ACTIVATION_SECRET = "PLANNING_PWA_SECRET_V1";
-
-async function sha256Hex(text) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(text);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-
-  return Array.from(new Uint8Array(hashBuffer))
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
-}
+import { generateActivationCode } from "../../tools/activation-algo.js";
 
 export async function checkActivation(codeSaisi, deviceId) {
   if (!codeSaisi || !deviceId) return false;
 
-  // RÈGLE EXACTE DU GÉNÉRATEUR
-  const expectedCode = (
-    await sha256Hex(`${ACTIVATION_SECRET}:${deviceId}`)
-  ).slice(0, 12);
+  const expectedCode = generateActivationCode(deviceId);
 
   return validateActivation(codeSaisi, expectedCode);
 }
