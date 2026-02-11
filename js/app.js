22abe9d218aecc2723da6642781e869ff971cc20
CHATELAIN JORDAN <jordan139@hotmail.fr>
2026-02-11 22:16:05 +0100

    Remove legacy app icons and keep only v5 set

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 /*
   Application Planningo
 */
-export const APP_VERSION = "2.0.101";
+export const APP_VERSION = "2.0.102";
 
 import {
   DB_VERSION,
@@ -583,6 +583,7 @@ function prewarmSecondaryViews() {
 
   setTimeout(preload, 1200);
 }
+
 
 
 
